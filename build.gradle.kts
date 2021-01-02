8c599be72042a3bf1cecef93838cb3381a6d321b
Denis Yakovlev <de1m0s242@gmail.com>
2021-01-03 03:25:59 +0700

    init project 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.1"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.openapi.generator") version "5.0.0"
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.spring") version "1.4.21"
 }
+
 
 group = "net.de1mos.ledger"
 version = "0.0.1-SNAPSHOT"
@@ -36,7 +38,16 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.liquibase:liquibase-core")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("io.r2dbc:r2dbc-postgresql:0.8.6.RELEASE")
+
+    api("io.springfox:springfox-swagger2:3.0.0")
+    api("io.springfox:springfox-swagger-ui:3.0.0")
+    api("io.swagger:swagger-annotations:1.6.2")
+    api("javax.validation:validation-api")
+    api("org.openapitools:jackson-databind-nullable:0.1.0")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner")
@@ -63,4 +74,35 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+val generatedSourcesDir = "$buildDir/generated/openapi"
+
+openApiGenerate {
+    generatorName.set("spring")
+    outputDir.set(generatedSourcesDir)
+    inputSpec.set("$rootDir/src/main/specs/ledger-recorder.yaml")
+    configOptions.put("apiPackage", "net.de1mos.ledger.recorder.api")
+    configOptions.put("modelPackage", "net.de1mos.ledger.recorder.api.models")
+    configOptions.put("bigDecimalAsString", "true")
+    configOptions.put("dateLibrary", "java8")
+    configOptions.put("reactive", "true")
+    configOptions.put("interfaceOnly", "true")
+    configOptions.put("skipDefaultInterface", "true")
+}
+
+sourceSets {
+    getByName("main") {
+        java {
+            srcDir("$generatedSourcesDir/src/main/java")
+        }
+    }
+}
+
+tasks {
+    val openApiGenerate by getting
+
+    val compileJava by getting {
+        dependsOn(openApiGenerate)
+    }
 }
