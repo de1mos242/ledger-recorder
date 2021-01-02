d565f93f9eb10cdfeacf75c0d275c055e77cb193
Denis Yakovlev <de1m0s242@gmail.com>
2021-01-03 03:32:24 +0700

    init project 3

--- src/main/kotlin/net/de1mos/ledger/recorder/controllers/RecordsControllerImpl.kt
+++ src/main/kotlin/net/de1mos/ledger/recorder/controllers/RecordsControllerImpl.kt
@@ -3,17 +3,28 @@ package net.de1mos.ledger.recorder.controllers
 import net.de1mos.ledger.recorder.api.RecordsApi
 import net.de1mos.ledger.recorder.api.models.Category
 import net.de1mos.ledger.recorder.api.models.Record
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.math.BigDecimal
 
-class RecordsControllerImpl: RecordsApi {
+@RestController
+class RecordsControllerImpl : RecordsApi {
     override fun recordsGet(exchange: ServerWebExchange?): Mono<ResponseEntity<Flux<Record>>> {
 //        return exchange.response.writeWith(Mono.just(Flux.just(Record().id("dd").category(Category().id("c1").name("valenky")).name("super v").value(
 //            BigDecimal.TEN))))
-        return Mono.empty()
+        return Mono.just(
+            ResponseEntity.ok(
+                Flux.just(
+                    Record()
+                        .id("dd")
+                        .category(Category().id("c1").name("valenky"))
+                        .name("super v")
+                        .value(BigDecimal.TEN)
+                )
+            )
+        )
     }
 }
